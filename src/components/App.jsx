e9cf91ce54301c5ef674ec2bfca147c7dc5a231a
Alexandr-Paleev <34216826+Alexandr-Paleev@users.noreply.github.com>
2020-04-10 17:16:55 +0300

    added button "Delete movie"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { moviesData } from '../moviesData';
+import MovieItem from './MovieItem';
 
 // UI = fn(state)
+
 
 class App extends React.Component {
     constructor() {
         super();
-
+        
         this.state = {
             movies: moviesData
-        }
+        };
+
+        // this.removeMovie = this.removeMovie.bind(this);
     }
+
+    removeMovie = (movie) => {
+        const updateMovie = this.state.movies.filter(function(item) {
+            return item.id !== movie.id;
+        });
+        console.log(updateMovie);
+        this.setState({
+            movies: updateMovie
+        });
+    }
+
     render() {
-        console.log(this)
-        return <div>
-            {this.state.movies.map((elem) => {
-                return <p>{elem.title}</p>
-            })}
-        </div>;
+        // console.log(this)
+        return (
+            <div>
+                {this.state.movies.map((movie) => {
+                    return <MovieItem key={movie.id} movie={movie} removeMovie={this.removeMovie} />;
+                })}
+            </div>
+        );
     }
 }
 

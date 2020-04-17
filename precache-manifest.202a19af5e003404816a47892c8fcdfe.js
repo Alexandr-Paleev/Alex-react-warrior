1fd6bd1d4d6afbc0673cd1ba55d8425bff47fe21
Alexandr-Paleev <34216826+Alexandr-Paleev@users.noreply.github.com>
2020-04-17 17:12:46 +0300

    Updates

---
+++ precache-manifest.202a19af5e003404816a47892c8fcdfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ef53c03304304ca51a9b9576184f52",
+    "url": "/Alex-react-warrior/index.html"
+  },
+  {
+    "revision": "e7ec06eddc7f9b4a84d3",
+    "url": "/Alex-react-warrior/static/css/2.7bf17230.chunk.css"
+  },
+  {
+    "revision": "a77dcf4b4e76c1235736",
+    "url": "/Alex-react-warrior/static/css/main.8f77a8e1.chunk.css"
+  },
+  {
+    "revision": "e7ec06eddc7f9b4a84d3",
+    "url": "/Alex-react-warrior/static/js/2.3a1f2e63.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Alex-react-warrior/static/js/2.3a1f2e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77dcf4b4e76c1235736",
+    "url": "/Alex-react-warrior/static/js/main.ae86a370.chunk.js"
+  },
+  {
+    "revision": "68aad6ccfe837e4f5bb0",
+    "url": "/Alex-react-warrior/static/js/runtime-main.93e8a104.js"
+  }
+]);
